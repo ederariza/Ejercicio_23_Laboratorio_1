9d5ee5c157e49e477d88aad90b98331d2f1b0f7c
Infromatica II <eder.ariza@udea.edu.co>
2020-10-11 02:21:41 -0500

    Ejercicio 23 - Laboratorio 1

--- main.cpp
+++ main.cpp
@@ -7,33 +7,76 @@ El MCM de 4 y 6 es: 12
 */
 
 #include <iostream>
-
 using namespace std;
+
+bool numero_primo(int);
 
 int main(){
 
-    int numero_A = 24;
-    int numero_B = 12;
-    int factor_A;
-    int factor_B;
+    int numero_A;
+    int numero_B;
     int mcm = 1;
+    bool primo_A ;
+    bool primo_B;
 
-    cout << "Hello World!" << endl;
+    //Captura de datos
+    cout << "Ingrese el numero A: ";
+    cin >> numero_A;
+    cout << "Ingrese el numero B: ";
+    cin >> numero_B;
 
-    while (true){
+    primo_A = numero_primo(numero_A);
+    primo_B = numero_primo(numero_B);
 
-        factor_A = numero_A / 2;
-        numero_A = factor_A;
-        mcm *= factor_A;
-        cout << mcm << endl;
+    //Primera condicion, si los numeros son pares y alguno es mcm del otro
+    if ((numero_A % numero_B == 0) || (numero_B % numero_A == 0))
 
-        if (numero_A == 1)
-            break;
+        if (numero_A % numero_B == 0)
+            mcm = numero_A,
+            cout << "El mcm entre " << numero_A << " y " << numero_B << " es " << mcm << endl;
 
-        else if (numero_A == 0)
-            break;
+        else
+            mcm = numero_B,
+            cout << "El mcm entre " << numero_A << " y " << numero_B << " es " << mcm << endl;
 
-    }
+    //Segunda condicion, si ambos numeros son primos
+    else if ((primo_A == true) && (primo_B == true))
+        mcm = numero_A * numero_B,
+        cout << "El mcm entre " << numero_A << " y el numero " << numero_B << " es " << mcm << endl;
+
+    //Tercera condicion un numero par y otro primo
+    else
+        mcm = numero_A * numero_B,
+        cout << "El mcm entre " << numero_A << " y el numero " << numero_B << " es " << mcm << endl;
 
     return 0;
+}
+
+//Funcion de numeros primos
+bool numero_primo(int numero){
+
+    bool primo;
+    int contador = 0;
+    for(int i=1; i<=numero; i++) {
+
+        if((numero % i) == 0) {
+                contador += 1;
+
+                if ((contador > 2) || (numero == 1)){
+                    primo = false;
+                    break;
+                }
+
+                else{
+                    if((contador == 2) && (i == numero))
+                        primo = true;
+                }
+        }
+
+        else{
+            continue;
+        }
+    }
+
+    return primo;
 }
